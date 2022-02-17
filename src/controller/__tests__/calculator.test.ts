623aa4b4247a3c9fe2b6e9e66cb83ab721c2f8ca
Suleiman Mamman <suleimamman@gmail.com>
2022-02-17 16:03:55 +0100

    method to handle new lines in the input format

--- src/controller/__tests__/calculator.test.ts
+++ src/controller/__tests__/calculator.test.ts
@@ -5,19 +5,35 @@ describe("Calculator Controller", () => {
     describe("add number", () => {
       
         it("should replace all the delimiters and add all the numbers", async () => {
-            const calculator =  Add("1,2,5");
-            expect(calculator).toBe(8);
-        });
-        
+            const calulate =  Add("1,2,5");
+            expect(calulate).toBe(8);
+        }); 
     });
 
     describe("empty string", () => {
       
         it("Empty strings should return 0", async () => {
-            const calculator =  Add(" ");
-            expect(calculator).toBe(0);
+            const calulate =  Add("");
+            expect(calulate).toBe(0);
         });
-        
+    });
+
+    describe("whitespace string", () => {
+      
+        it("string contains just whitespace should return 0", async () => {
+            const calulate =  Add(" ");
+            expect(calulate).toBe(0);
+       }); 
+    });
+
+    describe("new lines", () => {
+      
+        it("method to handle new lines in the input format", async () => {
+            const calulateOne =  Add("1\n,2,3");
+            const calulateTwo =  Add("1,\n2,4");
+            expect(calulateOne).toBe(6);
+            expect(calulateTwo).toBe(7);
+        }); 
     });
   
 });
